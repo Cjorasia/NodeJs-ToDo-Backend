c1232f5602b8000b851dadb82678d7999775687a
Edex01 <jorasiac@gmail.com>
2021-03-03 17:33:13 +0530

    Register User route created

--- myServer/routes/users.js
+++ myServer/routes/users.js
@@ -2,8 +2,25 @@ import express from "express";
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // router.get('/', function(req, res, next) {
+  //   res.send('respond with a resource');
+  // });
+
+
+// REgister User
+router.post('/register', async (req, res) => {
+  // User should secure password beforre sending
+
+  if (isEmpty(name) || isEmpty(password) 
+     || !isEmpty(email) || !isEmpty(profileURL) 
+     || isEmpty(gender) || toDate(dob) ===null){
+       res.json({
+         result: "failed",
+         data: {},
+         message: `name,password,gender must be not empty. Email,profileURL,dob must be in correct forat`
+       });
+       return;
+     }
 });
 
 export default router;
